16ca82b71c9e22bc8ef4f2843117dc074fd0fb70
Daniel <danielpae104@gmail.com>
2018-02-12 07:28:26 +0000

    done

--- draw.c
+++ draw.c
@@ -7,10 +7,20 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  if(x0 > x1){
+    int x_temp = x1;
+    int y_temp = y1;
+    x1 = x0;
+    x0 = x_temp;
+    y1 = y0;
+    y0 = y_temp;
+  }
+  printf("x0:%d, y0:%d, x1:%d, y1:%d\n", x0, y0, x1, y1); 
+
   int a = y1 - y0;
   int b = x1 - x0;
 
-  if(a * b > 0){
+  if(a > 0){
     if(b > a){
       int d = 2 * a - b;
       while(x0 < x1){
@@ -22,9 +32,9 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
 	x0++;
 	d += 2 * a;
       }
-    }else if(a > b){
+    }else{
       int d = a - (2 * b);
-      while(x0 < x1){
+      while(y0 < y1){
 	plot(s, c, x0, y0);
 	if(d < 0) {
 	  x0++;
@@ -35,16 +45,28 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
       }
     }
   }else{
-    int d = b - (2 * a);
-    while(x0 < x1){
-      plot(s, c, x0, y0);
-      if(d < 0) {
-	y0--;
-	d -= 2 * b;
+    if(b > (a * -1)){
+      int d = b - (2 * a);
+      while(x0 < x1){
+	plot(s, c, x0, y0);
+	if(d < 0) {
+	  y0--;
+	  d += 2 * b;
+	}
+	x0++;
+	d += 2 * a;
       }
-      x0++;
-      d += 2 * a;
-      printf("%d ", d);
+    }else{
+      int d = b * 2 - a;
+      while(y1 < y0){
+	plot(s, c, x0, y0);
+	if(d > 0) {
+	  x0++;
+	  d += 2 * a;
+	}
+	y0--;
+	d += 2 * b;
+      }
     }
   }
 }
--- main.c
+++ main.c
@@ -17,9 +17,27 @@ int main() {
 
   clear_screen(s);
 	
-  draw_line(15, 15, 300, 50, s, c);
-  draw_line(15, 15, 300, 400, s, c);
-  draw_line(50, 300, 300, 200, s, c);
+  draw_line(50, 50, 100, 125, s, c);
+  draw_line(150, 50, 100, 125, s, c);
+  draw_line(100, 125, 100, 190, s, c);
+  draw_line(50, 205, 100, 190, s, c);
+  draw_line(150, 205, 100, 190, s, c);
+  draw_line(100, 190, 60, 255, s, c);
+  draw_line(60, 255, 140, 255, s, c);
+  draw_line(140, 255, 100, 190, s, c);
+  draw_line(250, 50, 300, 125, s, c);
+  draw_line(350, 50, 300, 125, s, c);
+  draw_line(300, 125, 300, 220, s, c);
+  draw_line(250, 125, 300, 220, s, c);
+  draw_line(350, 125, 300, 220, s, c);
+  draw_line(260, 220, 260, 270, s, c);
+  draw_line(260, 270, 340, 270, s, c);
+  draw_line(340, 270, 340, 220, s, c);
+  draw_line(260, 220, 340, 220, s, c);
+  //draw_line(60, 190, 115, 240, s, c);
+  //draw_line(140, 190, 115, 240, s, c);
+  
+
 
   display(s);
   save_extension(s, "lines.png");
